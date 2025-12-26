6fd2047dd0c20985f72398e835e4f81d08a0d997
Phạm Tuấn Kiệt <167632480+TuanKiet1774@users.noreply.github.com>
2025-12-26 17:46:46 +0700

    Cập nhật kết nối

--- src/app.js
+++ src/app.js
@@ -6,23 +6,19 @@ import tagRoutes from "./routes/tag.route.js";
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Route test API
 app.get("/", (req, res) => {
   res.send("🚀 Code Showcase API is running!");
 });
 
-// Routes
+
 app.use("/api/topics", topicRoutes);
 app.use("/api/codes", codeRoutes);
 app.use("/api/tags", tagRoutes);
 
-// Middleware check if Code is published (optional)
-// Sử dụng cho các route GET /api/codes/* nếu muốn lọc tự động
 app.use("/api/codes", (req, res, next) => {
   if (!req.query.showUnpublished) {
     req.query.isPublished = true;
@@ -30,7 +26,6 @@ app.use("/api/codes", (req, res, next) => {
   next();
 });
 
-// Global error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: "Something went wrong" });
