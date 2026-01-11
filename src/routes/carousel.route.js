53f8f0cc20fcae8fb1e64b807faa1720d1dd33f0
Phạm Tuấn Kiệt <167632480+TuanKiet1774@users.noreply.github.com>
2026-01-12 02:24:57 +0700

    Chỉnh carousel route

--- src/routes/carousel.route.js
+++ src/routes/carousel.route.js
@@ -1,19 +1,17 @@
-// routes/carousel.route.js
 import express from "express";
 import {
   createCarousel,
-  getAllCarousels,
+  getCarousels, 
   getCarouselById,
   updateCarousel,
-  deleteCarousel
-} from "../controllers/carousel.controller.js"; // controllers bạn sẽ tạo
+  deleteCarousel,
+} from "../controllers/carousel.controller.js";
 
 const router = express.Router();
-
-router.post("/", createCarousel);          // CREATE
-router.get("/", getAllCarousels);         // READ ALL
-router.get("/:id", getCarouselById);      // READ ONE
-router.put("/:id", updateCarousel);       // UPDATE
-router.delete("/:id", deleteCarousel);    // DELETE
+router.post("/", createCarousel);
+router.get("/", getCarousels);
+router.get("/:id", getCarouselById);
+router.put("/:id", updateCarousel);
+router.delete("/:id", deleteCarousel);
 
 export default router;
